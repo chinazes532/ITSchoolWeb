0f0a0d938d4b1181f5b1a42f00833ff803663aaf
ivancernikov <chernikof50@gmail.com>
2026-04-05 09:59:14 +0300

    поместил моковые данные в файлы папки utils/ и прокинул через пропсы

--- frontend/src/pages/Price.jsx
+++ frontend/src/pages/Price.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
 import PriceTable from '../components/PriceTable'
 import "../styles/Price.css";
+import { priceData } from '../utils/price';
 
 export default function Price() {
-    const priceData = [
-        {
-            id: 1,
-            title: "Базовый",
-            description: "Для начинающих. Основы программирования",
-            price: 30000
-          },
-          {
-            id: 2,
-            title: "Стандарт",
-            description: "Frontend + проекты",
-            price: 50000
-          },
-          {
-            id: 3,
-            title: "Премиум",
-            description: "Полный курс + помощь с трудоустройством",
-            price: 80000
-          }
-    ]
   return (
     <div>
       <PriceTable  priceData={priceData}/>
---
+++ frontend/src/utils/price.js
@@ -0,0 +1,20 @@
+export const priceData = [
+    {
+        id: 1,
+        title: "Базовый",
+        description: "Для начинающих. Основы программирования",
+        price: 30000
+      },
+      {
+        id: 2,
+        title: "Стандарт",
+        description: "Frontend + проекты",
+        price: 50000
+      },
+      {
+        id: 3,
+        title: "Премиум",
+        description: "Полный курс + помощь с трудоустройством",
+        price: 80000
+      }
+]
